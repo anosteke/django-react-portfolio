48c0af03ce444635fbfeba09d453137626677816
anosteke <prw@live.fr>
2024-08-06 11:00:38 +0200

    Format update

--- frontend/src/components/Experiences/ExperienceCard.tsx
+++ frontend/src/components/Experiences/ExperienceCard.tsx
@@ -7,48 +7,48 @@ interface ExperienceCardProps {
   location: string;
   link: string;
   duration: string;
-  description: string;
+  description: string[];
   skills: string[];
 }
 
-const ExperienceCard = ({ title, company, location, link, duration, description, skills } : ExperienceCardProps) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleCard = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <div>
-        <div className={`experience-card`}>
-          <div className={`experience-header ${isOpen ? 'open' : ''}`}>
-            <div className="title-company">
-              <h2>{title} @ {company}</h2>
-            </div>
-            <div className="duration hide-on-small">{duration}</div>
-            <button className="toggle-button" onClick={toggleCard}>
-              {isOpen ? '-' : '+'}
-            </button>
+const ExperienceCard = ({ title, company, location, link, duration, description, skills }: ExperienceCardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCard = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <div className={`experience-card ${isOpen ? 'open' : ''}`}>
+        <div className={`experience-header ${isOpen ? 'open' : ''}`}>
+          <div className="title-company">
+            <h2>{title} @ {company}</h2>
+          </div>
+          <div className="duration hide-on-small">{duration}</div>
+          <button className="toggle-button" onClick={toggleCard}>
+            {isOpen ? '-' : '+'}
+          </button>
+        </div>
+      </div>
+      <div className={`experience-body ${isOpen ? 'open' : ''}`}>
+        <div className='complementary-information'>
+          <a href={`http://${link}`} className="company-link" target="_blank" rel="noopener noreferrer">{link}</a>
+          <p className="location"><i className="fas fa-map-marker-alt"></i> {location}</p>
+        </div>
+        <div className='description'>
+          {description.map((line, index) => (
+            <p key={index}>{line.trim()}</p>
+          ))}
+          </div>
+          <div className="skills">
+            {skills.map((skill, index) => (
+              <span key={index} className="skill-tag">{skill}</span>
+            ))}
           </div>
         </div>
-        {isOpen && (
-          <div className={`experience-card ${isOpen ? 'open' : ''}`}>
-            <div className='experience-body'>
-              <div className='complementary-information'>
-                <a href={`http://${link}`} className="company-link" target="_blank" rel="noopener noreferrer">{link}</a>
-                <p className="location"><i className="fas fa-map-marker-alt"></i> {location}</p>
-              </div>
-              <p className="description">{description}</p>
-              <div className="skills">
-                {skills.map((skill, index) => (
-                  <span key={index} className="skill-tag">{skill}</span>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
       </div>
-    );
+      );
   };
-  
-  export default ExperienceCard;
+
+      export default ExperienceCard;
