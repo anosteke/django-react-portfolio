34aa7cde19e6ff764a654709899e1b5b3e388455
anosteke <prw@live.fr>
2024-08-06 11:23:19 +0200

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Resumes from './components/Experiences/Resumes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Home></Home>
+    <BrowserRouter>
+    <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/resumes" element={<Resumes/>} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
