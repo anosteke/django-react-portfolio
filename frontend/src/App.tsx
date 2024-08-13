619f609255da08293c926f8161161215643b1492
anosteke <prw@live.fr>
2024-08-13 13:02:23 +0200

    Updated App with new components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Resumes from './components/Experiences/Resumes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
     <BrowserRouter>
     <Header></Header>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/resumes" element={<Resumes/>} />
+      <Route path="/about" element={<About/>} />
     </Routes>
+    <Footer></Footer>
     </BrowserRouter>
     </div>
   );
