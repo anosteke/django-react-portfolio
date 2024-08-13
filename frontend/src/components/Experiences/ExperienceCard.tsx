a30b6e432807717015ce6b37624738d7dd2e7b5a
anosteke <prw@live.fr>
2024-08-13 13:01:56 +0200

    Updated Experience Card

--- frontend/src/components/Experiences/ExperienceCard.tsx
+++ frontend/src/components/Experiences/ExperienceCard.tsx
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
 import '../../styles/experiencecard.scss';
+import { Url } from 'url';
 
-interface ExperienceCardProps {
-  title: string;
+export interface ExperienceCardProps {
+  job: string;
   company: string;
   location: string;
-  link: string;
-  duration: string;
-  description: string[];
-  skills: string[];
+  url: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  skills: JSON;
 }
 
-const ExperienceCard = ({ title, company, location, link, duration, description, skills }: ExperienceCardProps) => {
+const ExperienceCard = ({ job, company, location, url, startDate, endDate, description, skills }: ExperienceCardProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleCard = () => {
     setIsOpen(!isOpen);
   };
 
+  console.log(url)
+
   return (
     <div>
       <div className={`experience-card ${isOpen ? 'open' : ''}`}>
         <div className={`experience-header ${isOpen ? 'open' : ''}`}>
           <div className="title-company">
-            <h2>{title} @ {company}</h2>
+            <h2>{job} @ {company}</h2>
           </div>
-          <div className="duration hide-on-small">{duration}</div>
+          <div className="duration hide-on-small">{endDate ? `${startDate} - ${endDate}` : `${startDate} - now`}</div>
           <button className="toggle-button" onClick={toggleCard}>
             {isOpen ? '-' : '+'}
           </button>
@@ -33,16 +37,16 @@ const ExperienceCard = ({ title, company, location, link, duration, description,
       </div>
       <div className={`experience-body ${isOpen ? 'open' : ''}`}>
         <div className='complementary-information'>
-          <a href={`http://${link}`} className="company-link" target="_blank" rel="noopener noreferrer">{link}</a>
-          <p className="location"><i className="fas fa-map-marker-alt"></i> {location}</p>
+          <a href={url} className="company-link" target="_blank" rel="noopener noreferrer">{url.replace(/^https?\:\/\//i, "")}</a>
+          <p className="location"><i className="fas fa-map-marker-alt"></i>{location}</p>
         </div>
         <div className='description'>
-          {description.map((line, index) => (
-            <p key={index}>{line.trim()}</p>
+          {description.split('-').filter(line => line.trim() !== '').map((line, index) => (
+            <p key={index}>- {line.trim()}</p>
           ))}
           </div>
           <div className="skills">
-            {skills.map((skill, index) => (
+            {Object.values(skills).map((skill, index) => (
               <span key={index} className="skill-tag">{skill}</span>
             ))}
           </div>
