33e2a674f2e6dd7907770443dbdcb57ca2c9d468
anosteke <prw@live.fr>
2024-08-13 13:01:05 +0200

    Added an url field in Project and defined an __str__ return for readability

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 class Project(models.Model):
@@ -5,9 +6,11 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     skills = models.JSONField(default=list)
     imageUrl = models.URLField()
+    url = models.URLField()
 
     def __str__(self):
-        return self.titleclass Job(models.Model):
+        return self.title
+    
 class Job(models.Model):
     company = models.CharField(max_length=60)
     location = models.CharField(max_length=30, default="")
