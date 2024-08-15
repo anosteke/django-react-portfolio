ea0e23426b09d6519a5a27a54ef3ee5fcf01033b
anosteke <prw@live.fr>
2024-08-15 19:06:45 +0200

    Updated ProjectProps interface

--- frontend/src/components/Experiences/ProjectCard.tsx
+++ frontend/src/components/Experiences/ProjectCard.tsx
@@ -4,24 +4,24 @@ import '../../styles/project.scss';
 export interface ProjectProps
 {
     title : string;
-    url : string;
-    imgSrc : string;
     description : string;
-    skills : string[];
+    skills : JSON;
+    url : string;
+    imageUrl : string;
 }
 
-const Project = (project : ProjectCardProps) => {
+const Project = (project : ProjectProps) => {
 
 
   return (
     <div onClick = {() => window.open(project.url)} className="project-card width-on-small">
-        <img className="project-image" src={project.imgSrc}></img>
+        <img className="project-image" src={project.imageUrl}></img>
         <div className="project">
             <h3 className="project-title font-bold">{project.title}</h3>
             <p className="project-description">{project.description}</p>
             <div className="skills">
-                {project.skills.map(skill => (
-                    <span key={skill} className="skill">{skill}</span>
+                {Object.values(project.skills).map(skill => (
+                    <span className="skill">{skill}</span>
                 ))}
             </div>
         </div>
