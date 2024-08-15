eda081068d523f055ac29d817b19f076617f9baf
anosteke <prw@live.fr>
2024-08-15 18:58:02 +0200

    Added School Degrees

--- frontend/src/components/Experiences/School.tsx
+++ frontend/src/components/Experiences/School.tsx
@@ -1,33 +1,35 @@
 import { useState, useEffect } from "react";
 
 interface StudiesProps {
-    schoolName: string;
-    schoolImageURL: string;
-    duration: string;
-    description: string[];
-    degree : string
-    imagePadding : string
-  }
-  
+  schoolName: string;
+  schoolImageURL: string;
+  duration: string;
+  description: string[];
+  degree: string
+  imagePadding: string
+}
 
-const School = (studies : StudiesProps) => {
+
+const School = (studies: StudiesProps) => {
 
 
   return (
 
-          <div className="relative flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none grid gap-2 item sm:grid-cols-2">
-            <div className="relative bg-clip-border rounded-xl overflow-hidden bg-white text-gray-700"><img src={studies.schoolImageURL} alt="Revolutionizing Our Production Process" className={`object-scale-down w-full h-full p-${studies.imagePadding}`} /></div>
-            <div className="p-6 px-2 sm:pr-6 sm:pl-4">
-              <p className="block antialiased font-sans text-sm leading-normal text-gray-700 font-normal">{studies.duration}</p>
-              <p className="block antialiased font-sans text-sm font-light leading-normal text-inherit mb-4 !font-semibold">{studies.schoolName}</p><a href="#" className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 mb-2 normal-case transition-colors hover:text-gray-700">{studies.degree}</a>
-              <p className="block antialiased font-sans text-base leading-relaxed text-inherit mb-8 font-normal !text-gray-500"> <ul>
-      {studies.description.map((line, index) => (
-        <li key={index}>{line.trim()}</li>
-      ))}
-    </ul></p>
-                
-              </div>
-            </div>
+    <div className="relative flex-col bg-clip-border rounded-xl bg-transparent text-gray-700 shadow-none grid gap-2 item sm:grid-cols-2">
+      <div className="relative bg-clip-border rounded-xl overflow-hidden bg-white text-gray-700"><img src={studies.schoolImageURL} alt="Revolutionizing Our Production Process" className={`object-scale-down w-full h-full p-${studies.imagePadding}`} /></div>
+      <div className="p-6 px-2 sm:pr-6 sm:pl-4">
+        <div className="block antialiased font-sans text-sm leading-normal text-gray-700 font-normal">{studies.duration}</div>
+        <div className="block antialiased font-sans text-sm font-light leading-normal text-inherit mb-4 !font-semibold">{studies.schoolName}</div>
+        <a href="#" className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 mb-2 normal-case transition-colors hover:text-gray-700">{studies.degree}</a>
+        <div className="block antialiased font-sans text-base leading-relaxed text-inherit mb-8 font-normal !text-gray-500">
+          <div>
+            {studies.description.map((line, index) => (
+              <p key={index}>{line.trim()}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
 
   );
 };
--- frontend/src/components/Experiences/Studies.tsx
+++ frontend/src/components/Experiences/Studies.tsx
@@ -1,11 +1,30 @@
 import { useState, useEffect } from "react";
-import SteelSeriesImgSrc from "../../assets/images/steelserieslogo.png";
+import School from "./School";
+import '../../styles/studies.scss';
+
+const descriptionUQAC = `
+- Software development
+- Video games development
+- VR
+`.split('\n').filter(line => line.trim() !== '');
+
+
+const descriptionPolyech = `
+- Software developpement
+- Data Science
+`.split('\n').filter(line => line.trim() !== '');
 const Studies = () => {
 
 
   return (
-    <div className="jobs">
-      <h3 className="job-title font-extrabold">Academic Cursus 🎓</h3>
+    <div className="studies-container">
+      <h3 className="section-title font-extrabold">Academic Cursus 🎓</h3>
+      <section className="grid p-8 place-items-center">
+      <div className="container grid grid-cols-1 gap-8 my-auto lg:grid-cols-2">
+      <School degree={'MSc Computer Science'} description={descriptionUQAC} duration={"2022-2023"} schoolName="Université du Quebec à Chicoutimi, Canada" schoolImageURL="https://www.uqac.ca/dim/wp-content/uploads/2011/09/LOGO-UQAC575vC-7po.jpg" imagePadding={'50'}></School>
+      <School degree={'MEng Computer Science'} description={descriptionPolyech} duration={"2018-2023"} schoolName="Polytech Grenoble, France" schoolImageURL="https://i.hellowork.com/zfXAtPqBfhMXT7N0qiz4rgHZfRd3_oqJWlBSvAdyHZI/w:225/h:168/rt:fit/g:sm/bG9jYWw6Ly8vZWR0ZWNoL2V0YWJsaXNzZW1lbnQvbG9nby8yMDgwL2FmYmY2MDdkLTkzYmYtNGFjZi05ZTQ3LTI3ZjRmMTJmM2NiMS5zdmc.svg" imagePadding={'35'}></School>
+      </div>
+      </section>
     </div>
   );
 };
