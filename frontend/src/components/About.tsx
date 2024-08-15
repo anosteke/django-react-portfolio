b4d7f4d66258a5cad20152f146c116a6d726b9e1
anosteke <prw@live.fr>
2024-08-15 18:58:57 +0200

    Added About.tsx

---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+const About = () => {
+    return <div>
+        Hi! I'm Pierre-Raphaël, a 24-year-old enthusiastic software engineer with a profound passion for learning new things. Currently, I work at INSEE as a Java/React developer.
+        I have studied Software Engineering and Video Game Development in both France and Canada, gaining diverse experiences through various internships. These opportunities have allowed me to work in different fields, broadening my skill set and expertise.
+
+        This website was built using React for the frontend, thanks to its versatility. For the backend, I utilized the Django framework, which enables me to easily update my portfolio without needing to rebuild the front end. A SQLite database handles all my work, 
+        providing efficient and reliable data management.
+        
+        Feel free to contact me by clicking here or on the mail button in the page's header. You can also download my resume if you wish. I look forward to connecting with you!
+
+    </div>
+}
+
+export default About
+
