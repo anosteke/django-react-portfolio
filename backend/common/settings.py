02508bc5923fcb451b25fc645e5495d6189b0430
anosteke <prw@live.fr>
2024-08-15 19:33:01 +0200

    Added backend host

--- backend/common/settings.py
+++ backend/common/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=o@l$o-u$9h!dhq7+g0qu(#4@m!mr@+z91@!sjad_x)ba#5(_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prw.pythonanywhere.com']
 
 
 # Application definition
