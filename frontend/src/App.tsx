5a86ba3f5340e9ee8cad00d64ce44dd1a051fef2
anosteke <prw@live.fr>
2024-08-15 18:58:35 +0200

    Replaced BrowserRouter with HashRouter for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
 import Resumes from './components/Experiences/Resumes';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Footer from './components/Footer';
 
@@ -10,15 +10,15 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="app">
-    <BrowserRouter>
+    <HashRouter basename="/">
     <Header></Header>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/resumes" element={<Resumes/>} />
       <Route path="/about" element={<About/>} />
     </Routes>
     <Footer></Footer>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
