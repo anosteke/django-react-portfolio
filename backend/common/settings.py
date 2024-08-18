be3e73a27d39e88133407eeb0a6ffc8a293a5c76
anosteke <prw@live.fr>
2024-08-18 09:46:05 +0000

    Fixed origin syntax for api calls

--- backend/common/settings.py
+++ backend/common/settings.py
@@ -60,6 +60,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://pierreraphaelwozny.com",
+    "https://www.pierreraphaelwozny.com"
 ]
 
 # Optional settings
